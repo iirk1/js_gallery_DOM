f3ceccef7b6f3fc7a61629bf9d53e9515293d7c8
Iryna <irakogut23190@gmail.com>
2024-12-17 17:40:45 +0200

    make build

---
+++ index.a8bbb871.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll("a"),r=document.querySelector("#largeImg");Array.from(e).forEach(function(e){e.addEventListener("click",function(c){c.preventDefault(),r.src=e.href})});
+//# sourceMappingURL=index.a8bbb871.js.map
